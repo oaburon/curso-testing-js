34edad7227d26cd725986d5062a5f2a673fe6999
Oscar <oaburon@gmail.com>
2022-06-01 09:46:33 +0200

    Ejercicio 1

--- 1 - what is this/gift.test.js
+++ 1 - what is this/gift.test.js
@@ -1,5 +1,9 @@
 const gift = require('./gift');
 
 test('what is this?', () => {
-    expect(gift(5)).toBe('');
+    expect(gift(1)).toBe(null);
+    expect(gift(2)).toBe(null);
+    expect(gift(3)).toBe(' *\n***\n *\n');
+    expect(gift(4)).toBe(null);
+    expect(gift(5)).toBe('  *\n ***\n*****\n ***\n  *\n');
 });
